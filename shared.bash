9cc62408d1381476b11affd421dd6042dbe84473
Gabe Rosenhouse <grosenhouse@pivotal.io>
2017-08-28 16:59:28 -0700

    Add back bosh-lite helpers

--- shared.bash
+++ shared.bash
@@ -134,3 +134,110 @@ function reinstall() {
 
 main
 unset -f main
+
+
+gobosh_create_bosh_lite ()
+{
+    local env_dir=${HOME}/workspace/deployments/lite
+
+    bosh create-env ~/workspace/bosh-deployment/bosh.yml \
+    --state $env_dir/state.json \
+    -o ~/workspace/bosh-deployment/virtualbox/cpi.yml \
+    -o ~/workspace/bosh-deployment/virtualbox/outbound-network.yml \
+    -o ~/workspace/bosh-deployment/bosh-lite.yml \
+    -o ~/workspace/bosh-deployment/bosh-lite-runc.yml \
+    -o ~/workspace/bosh-deployment/jumpbox-user.yml \
+    --vars-store $env_dir/creds.yml \
+    -v director_name="Bosh Lite Director" \
+    -v internal_ip=192.168.50.6 \
+    -v internal_gw=192.168.50.1 \
+    -v internal_cidr=192.168.50.0/24 \
+    -v outbound_network_name="NatNetwork"
+
+    bosh -e 192.168.50.6 --ca-cert <(bosh int $env_dir/creds.yml --path /director_ssl/ca) alias-env vbox
+    BOSH_CLIENT="admin"
+    BOSH_CLIENT_SECRET="$(bosh int $env_dir/creds.yml --path /admin_password)"
+    BOSH_ENVIRONMENT="vbox"
+    BOSH_DEPLOYMENT="cf"
+    BOSH_CA_CERT="/tmp/bosh-lite-ca-cert"
+
+    export BOSH_CLIENT
+    export BOSH_CLIENT_SECRET
+    export BOSH_ENVIRONMENT
+    export BOSH_DEPLOYMENT
+    export BOSH_CA_CERT
+    bosh int $env_dir/creds.yml --path /director_ssl/ca > ${BOSH_CA_CERT}
+
+    STEMCELL_VERSION="$(bosh int ~/workspace/cf-deployment/cf-deployment.yml --path=/stemcells/0/version)"
+    echo "will upload stemcell ${STEMCELL_VERSION}"
+    bosh -e vbox upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=${STEMCELL_VERSION}"
+
+    bosh -e vbox -n update-cloud-config ~/workspace/cf-deployment/bosh-lite/cloud-config.yml
+}
+
+gobosh_delete_bosh_lite ()
+{
+    local env_dir=${HOME}/workspace/deployments/lite
+
+    bosh delete-env ~/workspace/bosh-deployment/bosh.yml \
+    --state $env_dir/state.json \
+    -o ~/workspace/bosh-deployment/virtualbox/cpi.yml \
+    -o ~/workspace/bosh-deployment/virtualbox/outbound-network.yml \
+    -o ~/workspace/bosh-deployment/bosh-lite.yml \
+    -o ~/workspace/bosh-deployment/bosh-lite-runc.yml \
+    -o ~/workspace/bosh-deployment/jumpbox-user.yml \
+    --vars-store $env_dir/creds.yml \
+    -v director_name="Bosh Lite Director" \
+    -v internal_ip=192.168.50.6 \
+    -v internal_gw=192.168.50.1 \
+    -v internal_cidr=192.168.50.0/24 \
+    -v outbound_network_name="NatNetwork"
+}
+
+gobosh_untarget ()
+{
+  unset BOSH_DIR
+  unset BOSH_USER
+  unset BOSH_PASSWORD
+  unset BOSH_ENVIRONMENT
+  unset BOSH_GW_HOST
+  unset BOSH_GW_PRIVATE_KEY
+  unset BOSH_CA_CERT
+  unset BOSH_DEPLOYMENT
+  unset BOSH_CLIENT
+  unset BOSH_CLIENT_SECRET
+}
+
+gobosh_target_lite ()
+{
+  gobosh_untarget
+  local env_dir=${HOME}/workspace/deployments/lite
+
+  pushd $env_dir >/dev/null
+    BOSH_CLIENT="admin"
+    BOSH_CLIENT_SECRET="$(bosh int ./creds.yml --path /admin_password)"
+    BOSH_ENVIRONMENT="vbox"
+    BOSH_CA_CERT=/tmp/bosh-lite-ca-cert
+
+    export BOSH_CLIENT
+    export BOSH_CLIENT_SECRET
+    export BOSH_ENVIRONMENT
+    export BOSH_CA_CERT
+    bosh int ./creds.yml --path /director_ssl/ca > $BOSH_CA_CERT
+  popd 1>/dev/null
+
+  export BOSH_DEPLOYMENT=cf;
+}
+
+
+gobosh_deploy_bosh_lite ()
+{
+  local env_dir=${HOME}/workspace/deployments/lite
+
+  bosh deploy --no-redact -n ~/workspace/cf-deployment/cf-deployment.yml \
+  -o ~/workspace/cf-deployment/operations/bosh-lite.yml \
+  -o ~/workspace/cf-deployment/operations/bypass-cc-bridge.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/disable-etcd.yml \
+  --vars-store $env_dir/deployment-vars.yml \
+  -v system_domain=bosh-lite.com
+}
